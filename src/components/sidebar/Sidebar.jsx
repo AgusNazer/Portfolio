feddb5f103afdbbfa77cba04d9cf7e35fca93313
AgusNazer <agustin.nazer@hotmail.com>
2023-08-30 02:39:50 -0300

    dark mode ulala

--- src/components/sidebar/Sidebar.jsx
+++ src/components/sidebar/Sidebar.jsx
@@ -2,9 +2,18 @@ import { NavLink } from "react-router-dom";
 import { AiFillHome, AiFillCode } from "react-icons/ai";
 import { FaOdnoklassniki, FaBookBookmark, FaMessage } from "react-icons/fa6";
 
+
+
+
+
+
+
 const Sidebar = () => {
+
+
   return (
     <div className="fixed left-0 bottom-0 md:left-0 w-full md:w-auto bg-gray-500 bg-opacity-30 md:bg-opacity-70 p-4 md:p-2 flex md:flex-col justify-center items-center gap-4 rounded-full rounded-bl-full md:rounded-tr-full md:rounded-br-full z-50 md:top-1/2 md:transform md:-translate-y-1/2">
+       
       <NavLink
         to="/#home-section"
         className="text-gray-100 md:text-gray-300 bg-gray-500 rounded-md px-3 py-2 text-2xl md:text-m font-medium"
--- tailwind.config.js
+++ tailwind.config.js
@@ -5,7 +5,11 @@ export default {
     "./src/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      backgroundColor: {
+        'dark-background': 'linear-gradient(45deg, #1b3547, #07090a, #0453a8, #292d2e, #f5cee1)', // Cambia este valor al color deseado
+      },
+    },
   },
   plugins: [],
 }
