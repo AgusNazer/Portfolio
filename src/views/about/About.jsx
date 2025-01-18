3639bfb455aa4161a816a2f0f9eb8dd489105c0f
agusNazer <agustin.nazer@hotmail.com>
2025-01-17 21:25:27 -0300

    sdfs

--- src/views/about/About.jsx
+++ src/views/about/About.jsx
@@ -1,6 +1,5 @@
-import avatarImg from '../../assets/images/avatar3.jpeg'
-// import PorfolioPic from '../../assets/images/PorfolioPic.jpg'
-import pic from "../../assets/images/PorfolioSinFondo.png"
+// import avatarImg from '../../assets/images/avatar3.jpeg'
+import pic from "../../assets/images/porfolioSinFondo.png"
 
 
 const About = () => {
@@ -9,9 +8,9 @@ const About = () => {
     <div className="flex flex-col-reverse md:flex-row w-full max-w-screen-lg mx-auto p-6 md:p-10 rounded-lg shadow-lg">
       <div className="w-48 h-48 md:mb-0 md:mr-6 ml-auto mr-auto">
         <img
-          src={avatarImg}
+          // src={avatarImg}
           // src={PorfolioPic}
-          // src={pic}
+          src={pic}
           alt="Avatar"
           className=" w-full h-full object-cover rounded-full border-1 border-blue-900 border-opacity-75 shadow-sm"
         />
